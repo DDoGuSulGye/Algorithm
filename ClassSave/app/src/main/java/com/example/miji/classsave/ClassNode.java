98a272d3b691bcf995c5b47893a6e991e40a0261
권도경 <dkdk6638@gmail.com>
2018-12-01 17:49:46 +0900

    ClassNode Class Change
    to miji..for you..

--- ClassSave/app/src/main/java/com/example/miji/classsave/ClassNode.java
+++ ClassSave/app/src/main/java/com/example/miji/classsave/ClassNode.java
@@ -4,32 +4,15 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class ClassNode implements Serializable {
+
     String ClassName;
-    ArrayList<Integer> monday;
-    ArrayList<Integer> tuesday;
-    ArrayList<Integer> wednesday ;
-    ArrayList<Integer> thursday ;
-    ArrayList<Integer> friday ;
-
-//    ArrayList<Integer> monday = new ArrayList<Integer>();
-//    ArrayList<Integer> tuesday = new ArrayList<Integer>();
-//    ArrayList<Integer> wednesday = new ArrayList<Integer>();
-//    ArrayList<Integer> thursday = new ArrayList<Integer>();
-//    ArrayList<Integer> friday = new ArrayList<Integer>();
-
+    ArrayList<Integer> monday = new ArrayList<>();
+    ArrayList<Integer> tuesday  = new ArrayList<>();
+    ArrayList<Integer> wednesday  = new ArrayList<>();
+    ArrayList<Integer> thursday  = new ArrayList<>();
+    ArrayList<Integer> friday  = new ArrayList<>();
     String nearbyElevator;
-
-    public void ClassNode(){
-        ClassName = "";
-        //       monday = new ArrayList<Integer>();
-//        tuesday = new ArrayList<Integer>();
-//        tuesday.clear();
-//        wednesday.clear();
-//        thursday.clear();
-//        friday.clear();
-
-        nearbyElevator = "";
-    }
+    String stair;
 
     public String getClassName(){
         return ClassName;
@@ -59,32 +42,43 @@ public class ClassNode implements Serializable {
         return nearbyElevator;
     }
 
+    public String getStair() {
+        return stair;
+    }
+
+
+
     public void setClassName(String ClassName){
         this.ClassName = ClassName;
     }
 
     public void setMonday(ArrayList<Integer> monday){
-        this.monday = monday;
+        //this.monday = monday;
+        this.monday.addAll(monday);
     }
 
     public void setTuesday(ArrayList<Integer> tuesday){
-        this.tuesday = tuesday;
+        this.tuesday.addAll(tuesday);
     }
 
     public void setWednesday(ArrayList<Integer> wednesday){
-        this.wednesday = wednesday;
+        this.wednesday.addAll(wednesday);
     }
 
     public void setThursday(ArrayList<Integer> thursday){
-        this.thursday = thursday;
+        this.thursday.addAll(thursday);
     }
 
     public void setFriday(ArrayList<Integer> friday){
-        this.friday = friday;
+        this.friday.addAll(friday);
     }
 
     public void setNearbyElevator(String nearbyElevator){
         this.nearbyElevator = nearbyElevator;
+    }
+
+    public void setStair(String stair){
+        this.stair = stair;
     }
 
 }
